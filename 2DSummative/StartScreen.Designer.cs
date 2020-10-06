88d796589099bd8267aff715b2df58e17e9c08c8
Jordan Sinclair <jordsinc828@outlook.com>
2020-10-06 12:04:40 -0400

    finshed

--- 2DSummative/StartScreen.Designer.cs
+++ 2DSummative/StartScreen.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -36,7 +37,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.ControlText;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(202, 181);
+            this.button1.Location = new System.Drawing.Point(200, 222);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(199, 126);
             this.button1.TabIndex = 0;
@@ -44,21 +45,34 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "World\'s Easiest Game";
+            // 
             // StartScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "StartScreen";
             this.Size = new System.Drawing.Size(626, 472);
             this.Load += new System.EventHandler(this.StartScreen_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
